0be3ed24f959417b181145d689aeb3ddcd5e4b61
Nicholas <nics-lohr@gmx.de>
2018-04-26 22:20:07 +0200

    really calls the program

--- main.cpp
+++ main.cpp
@@ -19,12 +19,41 @@
 int main(int argc, char *argv[])
 {
 	packing pack;
-	pack.read_inst_from(argv[1]);
-	pack.read_sol_from(argv[2]);
-	if(pack.init_bmp())
+	if(argc < 3)
 	{
-		pack.draw_all_rectangles();
-		pack.draw_all_pins();
-		pack.write_bmp();
+		throw std::runtime_error("Insufficient arguments. Usage: ./programname instance_file solution_file");
+	}
+
+	pack.read_dimension_from_inst(argv[1]);
+	pack.read_sol_from(argv[2]);
+
+	std::pair<int, int> cert = pack.is_valid();
+
+	if(cert.first == -1)
+	{
+		std::cout << "No intersections found." << std::endl;
+	}
+	else
+	{
+		std::cout << "Rectangles " << cert.first << " and " << cert.second << " intersect." << std::endl;
+	}
+
+	if(argc > 3 && strcmp(argv[3], "-b") == 0)
+	{
+		if(pack.init_bmp())
+		{
+			pack.draw_all_rectangles();
+			pack.draw_all_pins();
+			pack.draw_cert(cert);
+			pack.write_bmp();
+		}
+		else
+		{
+			std::cout << "Instance is too big for a bitmap." << std::endl;
+		}
+	}
+	else
+	{
+		std::cout << "Run again with flag -b for a beautiful bitmap" << std::endl;
 	}
 }
