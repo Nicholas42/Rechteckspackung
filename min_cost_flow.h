79e1b46429dc1785406bc01704c7373fb47b809f
Nicholas <nics-lohr@gmx.de>
2018-05-25 18:20:50 +0200

    Added class for Min-Cost-Flow calculation. Still incomplete.

---
+++ min_cost_flow.h
@@ -0,0 +1,52 @@
+#ifndef RECHTECKSPACKUNG_MIN_COST_FLOW_H
+#define RECHTECKSPACKUNG_MIN_COST_FLOW_H
+
+#include <vector>
+#include <list>
+#include <cassert>
+#include <limits>
+#include <algorithm> //min_element
+
+using weight = int;
+
+
+// TODO: one could save memory by putting everything short of id and reverse in vector
+struct edge
+{
+    size_t from;
+    size_t to;
+    size_t id;
+    weight cost;
+    bool reverse;
+};
+
+struct node
+{
+    weight demand;
+    std::list<edge> adjacent;
+};
+
+using adjlist = std::vector<node>;
+using path = std::list<edge>;
+
+class graph
+{
+public:
+    static graph make_graph();
+
+    void augpath(path p, weight w = 0);
+    weight potential_cost(const edge &e) const;
+    void compute_min_flow();
+
+private:
+    adjlist _list;
+    std::vector<weight> _potential;
+    std::vector<weight > _flow;
+    path compute_shortest_path(size_t from);
+    void compute_starting_potential();
+
+    constexpr size_t _invalid_index = std::numeric_limits<size_t>::max();
+    constexpr  weight _invalid_cost = std::numeric_limits<weight>::max();
+};
+
+#endif //RECHTECKSPACKUNG_MIN_COST_FLOW_H
